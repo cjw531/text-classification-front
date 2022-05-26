eb6537bb0fe19c35b7fe6d178dd4c99a57f49267
Jiwon Choi <cjwon531@gmail.com>
2022-05-26 16:50:25 -0500

    updated frontend streamlit

--- app.py
+++ app.py
@@ -14,31 +14,35 @@ def send_request(text, type):
 
 st.title("Performance Comparison of Binary and Multi Class Text Classification Models With CNN & BERT + CNN Models")
 
-col1, col2 = st.columns(2) # set up 2 columns
+# col1, col2 = st.columns(2) # set up 2 columns
 
-col1.header("Binary Classification") # binary classification section
-col1.body("The pre-trained binary classification CNN and BERT + CNN models will classify whether it is a disaster-related Tweets or not.")
+st.header("Binary Classification") # binary classification section
+st.markdown("The pre-trained binary classification CNN and BERT + CNN models will classify whether it is a disaster-related Tweets or not.")
 
-binary_base_text = col1.text_input("Type Disaster or Non-Disaster Tweets", "Last week, around 21 people were assasinated in Texas due to mass shooting.")
-if col1.button("Submit"):
+binary_base_text = st.text_input("Type Disaster or Non-Disaster Tweets", "Last week, around 21 people were assasinated in Texas due to mass shooting.")
+if st.button("Predict Binary"):
     text = binary_base_text.title()
-    status_code, response = send_request(text, '1')
+    status_code, response = send_request(text, "1")
+    print(status_code)
+    print(response)
     if status_code == 200:
         prediction = response.json()
-        col1.success(prediction["prediction"])
+        st.success(prediction["CNN prediction"] + '\t' + prediction['BERT prediction'])
     else:
-        col1.error(str(status_code) + " Error")
+        st.error(str(status_code) + " Error")
 
-col2.header("Multiclass Classification")
-col2.body("The pre-trained multiclass classification CNN and BERT + CNN models will classify the news article into 7 categories: \
+st.markdown("""---""")
+
+st.header("Multi Classification")
+st.markdown("The pre-trained multiclass classification CNN and BERT + CNN models will classify the news article into 7 categories: \
     'automobile', 'entertainment', 'politics', 'science', 'sports', 'technology', 'world'")
 
-multi_base_text = col2.text_input("Type News Article to Classify", "Joe Biden recently visited South Korea and then he headed to Japan.")
-if col2.button("Submit"):
+multi_base_text = st.text_input("Type News Article to Classify", "Joe Biden recently visited South Korea and then he headed to Japan.")
+if st.button("Predict Multi"):
     text = multi_base_text.title()
     status_code, response = send_request(text, '2')
     if status_code == 200:
         prediction = response.json()
-        col2.success(prediction["prediction"])
+        st.success(prediction["CNN prediction"] + '\t' + prediction['BERT prediction'])
     else:
-        col2.error(str(status_code) + " Error")
+        st.error(str(status_code) + " Error")
